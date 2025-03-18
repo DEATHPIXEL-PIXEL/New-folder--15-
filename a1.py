84601ed7d2c1cfdca3d63edd2ea0d8e2b445d0f2
DEATHPIXEL-PIXEL <zayaankhan337@gmail.com>
2025-03-18 15:15:17 +0530

    Co-authored-by: Shrey Mishra <shreymishra13@users.noreply.github.com>

--- a1.py
+++ a1.py
@@ -1,8 +1,36 @@
 from tkinter import*
 root=Tk()
-root.title=("logi app")
+root.title=("login app")
 root.geometry("400x400")
 frame=Frame(master=root,height=250,width=360, bg="#136ce4" )
 
 
-l
+lbl1=Label(frame, text="Full ame ", bg="#136ce6" , fg="white" , width=12 )
+lbl2=Label(frame, text="email id ", bg="#136ce6" , fg="white" , width=12 )
+lbl3=Label(frame, text="password ", bg="#136ce6" , fg="white" , width=12 )
+
+name_entry=Entry(frame)
+email_entry=Entry(frame)
+pass_entry=Entry(frame,show="*")
+
+
+def display():
+    name=name_entry.get()
+    message=name + "account has been created"
+    textbox.insert(END,message)
+
+textbox= Text(bg="#BEBEBE" , fg="black")
+btn =Button(text="create accout" , command=display,bg="red")
+
+
+frame.place(x=  20,y=0)
+lbl1.place(x=20,y=20)
+name_entry.place(x=150, y=20)
+lbl2.place(x=20, y=80)
+email_entry.place(x=150, y=80)
+lbl3.place(x=20, y=140)
+pass_entry.place(x=150, y=140)
+btn.place(x=130, y=210)
+textbox.place(y=250)
+
+root.mainloop()
